a3506734a44c464c875e0d6079d01c1e9665c5ba
emma t <emma@scape.io>
2019-01-28 17:25:55 +0000

    bump scapekit version

--- scapekit-arsession/app/src/main/java/com/scape/scapekit/MainActivity.kt
+++ scapekit-arsession/app/src/main/java/com/scape/scapekit/MainActivity.kt
@@ -3,13 +3,24 @@ package com.scape.scapekit
 import android.os.Bundle
 import android.support.v4.app.FragmentActivity
 import android.util.Log
+import android.widget.Toast
 import com.google.ar.sceneform.ux.ArFragment
-import com.scape.scapekit.*
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : FragmentActivity() {
+/**
+ * Activity that demonstrates the use of ScapeKit.
+ *
+ * In order to display the AR preview we are grabbing an ArSession with `ArSession.withArFragment(sceneform_fragment)`.
+ *
+ * On `localize_button` button press we attempt to retrieve the current geoposition(Position and Orientation) via `ScapeSession.getCurrentGeoPose`
+ * with `GeoSourceType.RAWSENSORSANDSCAPEVISIONENGINE` flag to ensure a very precise localization.
+ *
+ */
+class MainActivity : FragmentActivity(), ScapeSessionObserver, ArSessionObserver {
+    val TAG = MainActivity::class.java.simpleName
+
     private var arSession: ArSession? = null
-    private var geoSession: GeoSession? = null
+    private var scapeSession: ScapeSession? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,15 +50,53 @@ class MainActivity : FragmentActivity() {
         super.onDestroy()
     }
 
+    override fun onScapeSessionClosed(p0: ScapeSession?, details: ScapeSessionDetails) {
+        Log.d(TAG, "ScapeSessionClosed: $details")
+    }
+
+    override fun onGeoPoseEstimated(p0: ScapeSession?, details: ScapeSessionDetails) {
+        Log.d(TAG, "Retrieving GPS LocationCoordinates: ${details.mGeoPose}")
+    }
+
+    override fun onScapeSessionStarted(p0: ScapeSession?, details: ScapeSessionDetails) {
+        Log.d(TAG, "ScapeSessionStarted: $details")
+    }
+
+    override fun onScapeSessionError(p0: ScapeSession?, details: ScapeSessionDetails) {
+        Log.d(TAG, "Could not retrieve geo coordinates: ${details.errorMessage}")
+    }
+
+    override fun onTrackingStateUpdated(p0: ArSession?, details: ArTrackingState) {
+        Log.d(TAG, "onTrackingStateUpdated: $details")
+    }
+
+    override fun onPlaneDetected(p0: ArSession?, details: ArPlane) {
+        Log.d(TAG, "onPlaneDetected: $details")
+    }
+
+    override fun onFrameUpdated(p0: ArSession?, details: ArFrame) {
+        Log.d(TAG, "onFrameUpdated: $details")
+    }
+
+    override fun onPlaneUpdated(p0: ArSession?, details: ArPlane) {
+        Log.d(TAG, "onPlaneUpdated: $details")
+    }
+
+    override fun onPlaneRemoved(p0: ArSession?, details: ArPlane) {
+        Log.d(TAG, "onPlaneRemoved: $details")
+    }
+
     private fun setupCamera() {
         arSession = ArSessionApp.sharedInstance.scapeClient.arSession?.withArFragment(sceneform_fragment as ArFragment)
         arSession?.isDebugMode = false
         arSession?.isPlaneDetection = true
         arSession?.isLightEstimation = true
+        arSession?.arSessionObserver = this
     }
 
     private fun setupGeo() {
-        geoSession = ArSessionApp.sharedInstance.scapeClient.geoSession
+        scapeSession = ArSessionApp.sharedInstance.scapeClient.scapeSession
+        scapeSession?.scapeSessionObserver = this
     }
 
     private fun bindings() {
@@ -57,43 +106,36 @@ class MainActivity : FragmentActivity() {
     }
 
     private fun getCurrentPositionAsync() {
-        geoSession?.getCurrentGeoPosition(
-                positionRawEstimated = { details ->
-                    val coordinates = "${details.rawLocation.coordinates.latitude} ${details.rawLocation.coordinates.longitude} ${details.rawLocation.coordinates.altitude}"
-                    Log.d("PXMainActivity", "Retrieving GPS LocationCoordinates: $coordinates")
-                },
-                positionLocked = { details ->
-                    val coordinates = "${details.lockedCoordinates.latitude} ${details.lockedCoordinates.longitude}"
-                    Log.d("PXMainActivity", "Retrieving Scape GeoCoordinates: $coordinates")
-                },
-                sessionError = { details ->
-                    Log.e("PXMainActivity", "Could not retrieve geo coordinates: ${details.errorMessage}")
-                })
+        scapeSession?.getCurrentGeoPose(GeoSourceType.RAWSENSORSANDSCAPEVISIONENGINE)
     }
 
+    /**
+     * Example on how to start continuous geoposition fetching using Scape Vision Engine.
+     */
     private fun startFetch() {
-        geoSession?.startFetch(
-                sessionStarted = {
-                    Log.d("PXMainActivity", "Session started")
-                },
-                positionRawEstimated = {
-
-                },
-                positionLocked = {
-
-                },
-                sessionError = {
-                    Log.d("PXMainActivity", "Session error ${it.errorMessage}")
-                })
+        scapeSession?.startFetch(GeoSourceType.RAWSENSORSANDSCAPEVISIONENGINE)
     }
 
+    /**
+     * Example on how to stop continuous geoposition fetching.
+     */
     private fun stopFetch() {
-        geoSession?.stopFetch(
+        scapeSession?.stopFetch(
                 sessionClosed = {
-                    Log.d("PXMainActivity", "Session stopped")
+                    Log.d(TAG, "Session stopped")
                 },
                 sessionError = {
-                    Log.d("PXMainActivity", "Session error ${it.errorMessage}")
+                    Log.d(TAG, "Session error ${it.errorMessage}")
                 })
     }
+
+    /**
+     * Display long toast  to notify the user of progress.
+     *
+     * @param message The toast message.
+     */
+    private fun displayToast(message: String) {
+        runOnUiThread { Toast.makeText(applicationContext, "$TAG:$message", Toast.LENGTH_LONG).show() }
+    }
+
 }
